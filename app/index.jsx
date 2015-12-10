8b6c8ae97a2874ab60632f4dd732089ba0dee2c8
uri.nativ <uri.nativ@klarna.com>
2015-12-11 00:17:13 +0200

    Implementing a Basic Note Application

---
+++ app/index.jsx
@@ -0,0 +1,17 @@
+import './main.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import 'array.prototype.findindex';
+
+main();
+
+function main() {
+  const app = document.createElement('div');
+
+  document.body.appendChild(app);
+
+  ReactDOM.render(<App />, app);
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,65 @@
 var path = require('path');
+
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+process.env.BABEL_ENV = TARGET;
+
+var common = {
   entry: PATHS.app,
-  output: {
-      path: PATHS.build,
-      filename: 'bundle.js'
-    },
-  devServer: {
+	resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      },
+  		{
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    // important! move HotModuleReplacementPlugin below
+    //new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
+};
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
       historyApiFallback: true,
       hot: true,
       inline: true,
       progress: true,
-  
+
       // display only errors to reduce the amount of output
       stats: 'errors-only',
-      
+
       // parse host and port from env so this is easy
       // to customize
       host: process.env.HOST,
       port: process.env.PORT
-  },
-  plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new HtmlwebpackPlugin({
-            title: 'Kanban app'
-          })
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
     ]
-};
+  });
+}
+
