be0d1cd1722f9aae405f4761a7f6143eb9fc8fee
uri.nativ <uri.nativ@klarna.com>
2015-12-14 00:36:22 +0200

    Removed empty line

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -9,9 +9,9 @@ export default class Notes extends React.Component {
 
   render() {
     const notes = this.props.items;
-
     return <ul className="notes">{notes.map(this.renderNote)}</ul>;
   }
+
   renderNote(note) {
     return (
       <li className="note" key={note.id}>
